99a89523405d3d7b6096cd0b45c721c88809cd51
thiagomartendal <thiagomartendal@gmail.com>
2026-03-06 19:39:01 -0300

    Add files via upload

---
+++ Curso/35 - Integração de Aplicações Externas - Listagem Web Client HTML5 - JavaScript/meu-projeto-springboot-listagem-web/src/main/java/com/produtoapi/MeuProjetoSpringbootListagemWebApplication.java
@@ -0,0 +1,13 @@
+package com.produtoapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeuProjetoSpringbootListagemWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeuProjetoSpringbootListagemWebApplication.class, args);
+	}
+
+}
---
+++ Curso/36 - Integração de Aplicações Externas - CRUD Web Client HTML5 - Javascript/meu-projeto-springboot-crud-web/src/main/java/com/produtoapi/MeuProjetoSpringbootCrudWebApplication.java
@@ -0,0 +1,13 @@
+package com.produtoapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeuProjetoSpringbootCrudWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeuProjetoSpringbootCrudWebApplication.class, args);
+	}
+
+}
